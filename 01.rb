e624c822f54bdbb15e7ee90fa950f72505ff8bd7
igor <igor@igor.io>
2020-12-01 22:11:05 +0100

    day 1, part 2

--- 01.rb
+++ 01.rb
@@ -23,3 +23,18 @@ numbers.each do |n|
 end
 
 puts res.inspect
+
+res = nil
+numbers.each do |n|
+  numbers.each do |m|
+    numbers.each do |l|
+      next if n == m || n == l || m == l
+      if n + m + l == 2020
+        res = n * m * l
+        break 2
+      end
+    end
+  end
+end
+
+puts res.inspect
