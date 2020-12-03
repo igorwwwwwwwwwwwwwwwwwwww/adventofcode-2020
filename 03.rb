23a9317241d1a152af76a40a874f7c8aaa4ec95b
igor <igor@igor.io>
2020-12-03 20:51:12 +0100

    day 3, part 2

--- 03.rb
+++ 03.rb
@@ -37,3 +37,28 @@ while map[y]
 end
 
 puts trees
+
+# part 2
+
+pairs = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+tree_counts = pairs.map do |dx, dy|
+  trees = 0
+  x = 0
+  y = 0
+
+  while map[y]
+    val = map[y][x]
+    if val == '#'
+      trees += 1
+    end
+
+    x = (x + dx) % cycle
+    y += dy
+  end
+
+  trees
+end
+
+# puts tree_counts.inspect
+puts tree_counts.reduce(:*)
