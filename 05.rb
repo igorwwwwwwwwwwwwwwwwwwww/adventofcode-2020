fcf22133c72a7e851b5fc5af314ab835004d47fe
igor <igor@igor.io>
2020-12-05 12:19:31 +0100

    day 5, part 1

---
+++ 05.rb
@@ -0,0 +1,47 @@
+input = <<EOF
+FBFBBFFRLR
+BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL
+EOF
+
+input = File.read("05.txt")
+
+passes = input.lines.map { |line|
+  chars = line.strip.chars
+
+  lower = 0
+  upper = 127
+  chars.slice(0, 7).each_with_index do |c, i|
+    case c
+    when 'F'
+      upper = upper - (1<<(7-i-1))
+    when 'B'
+      lower = lower + (1<<(7-i-1))
+    else
+      raise "invalid direction #{c}"
+    end
+  end
+  raise "upper and lower mismatch: #{upper}, #{lower}" unless upper == lower
+  row = lower
+
+  lower = 0
+  upper = 7
+  chars.slice(7, 10).each_with_index do |c, i|
+    case c
+    when 'L'
+      upper = upper - (1<<(3-i-1))
+    when 'R'
+      lower = lower + (1<<(3-i-1))
+    else
+      raise "invalid direction #{c}"
+    end
+  end
+  raise "upper and lower mismatch: #{upper}, #{lower}" unless upper == lower
+  col = lower
+
+  [row, col, row*8+col]
+}
+
+# puts passes
+puts passes.map { |row, col, seat| seat }.max
