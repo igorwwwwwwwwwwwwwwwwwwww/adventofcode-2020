f43ce3bd102deccbca495c8cfb1b3b9d3973add4
igor <igor@igor.io>
2020-12-06 16:50:41 +0100

    day 6, part 1

---
+++ 06.rb
@@ -0,0 +1,25 @@
+input = <<EOF
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+EOF
+
+input = File.read("06.txt")
+
+groups = input.split("\n\n")
+
+# part 1
+
+puts groups.map { |group| group.lines.map(&:strip).map(&:chars).flatten.uniq.size }.sum
