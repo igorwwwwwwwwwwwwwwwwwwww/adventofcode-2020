7a3521bd2c9f530b7fc76de5dba6c92c439e52c5
igor <igor@igor.io>
2020-12-08 19:53:26 +0100

    day 8, part 2

--- 08.rb
+++ 08.rb
@@ -18,10 +18,10 @@ code = input.lines
     { op: op, offset: offset.to_i }
   }
 
+# part 1
+
 acc = 0
 ip = 0
-
-# part 1
 
 while code[ip]
   # puts [ip, code[ip], acc].inspect
@@ -44,3 +44,64 @@ while code[ip]
 end
 
 puts acc
+
+# part 2
+
+code.each do |instr|
+  instr.delete(:seen)
+end
+
+def self.with_permutations(code)
+  code.each_with_index do |instr, i|
+    if instr[:op] == 'jmp'
+      instr[:op] = 'nop'
+
+      yield i
+
+      instr[:op] = 'jmp'
+    elsif instr[:op] == 'nop'
+      instr[:op] = 'jmp'
+
+      yield i
+
+      instr[:op] = 'nop'
+    end
+  end
+end
+
+class ProbablyInfiniteLoopException < StandardError; end
+
+def run_vm(code)
+  acc = 0
+  ip = 0
+  i = 0
+
+  while code[ip]
+    # puts [ip, code[ip], acc, i].inspect
+
+    instr = code[ip]
+    if i >= 1_000
+      raise ProbablyInfiniteLoopException
+    end
+
+    case instr[:op]
+    when 'nop'
+      ip += 1
+    when 'acc'
+      acc += instr[:offset]
+      ip += 1
+    when 'jmp'
+      ip += instr[:offset]
+    end
+
+    i += 1
+  end
+
+  acc
+end
+
+with_permutations(code) do |i|
+  # puts "permuting #{i}"
+  puts run_vm(code)
+rescue ProbablyInfiniteLoopException => e
+end
