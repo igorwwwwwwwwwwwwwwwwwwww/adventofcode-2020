d71d6cfd831b81cd7f1db6de0e1a505540651ab9
igor <igor@igor.io>
2020-12-13 12:48:00 +0100

    day 13, part 2, brute force

--- 13.rb
+++ 13.rb
@@ -5,10 +5,10 @@ EOF
 
 input = File.read("13.txt")
 
+# part 1
+
 earliest_t = input.lines[0].to_i
 bus_lines = input.lines[1].split(',').reject { |v| v == 'x' }.map(&:to_i)
-
-# part 1
 
 current_t = earliest_t
 until found = bus_lines.find { |v| current_t % v == 0 }
@@ -16,3 +16,20 @@ until found = bus_lines.find { |v| current_t % v == 0 }
 end
 
 puts (current_t-earliest_t)*found
+
+# part 2
+
+bus_lines = input.lines[1].split(',').each_with_index.map { |v, i| if v == 'x' then [nil, i] else [v.to_i, i] end }
+
+# puts bus_lines.inspect
+
+current_t = 0
+bus_lines.each do |v, i|
+  current_t = v-i if v && v-i > current_t
+end
+
+until bus_lines.all? {|v, i| !v || (current_t+i) % v == 0 }
+  current_t += 1
+end
+
+puts current_t
