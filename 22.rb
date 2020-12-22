6645f38dde87f66c97cd457e997792672f944c31
igor <igor@igor.io>
2020-12-22 13:00:24 +0100

    day 22, part 2
    $ time ruby 22.rb
    34424
    ruby 22.rb  22.39s user 0.09s system 99% cpu 22.638 total

--- 22.rb
+++ 22.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 input = <<EOF
 Player 1:
 9
@@ -14,7 +16,20 @@ Player 2:
 10
 EOF
 
+# input = <<EOF
+# Player 1:
+# 43
+# 19
+#
+# Player 2:
+# 2
+# 29
+# 14
+# EOF
+
 input = File.read("22.txt")
+
+# part 1
 
 players = input.split("\n\n").map { |p| p.lines.map(&:strip)[1..-1].map(&:to_i) }
 num_cards = players.flat_map(&:size).sum
@@ -29,6 +44,41 @@ end
 
 puts players
   .find { |p| p.size > 0 }
+  .each_with_index
+  .map { |v, i| v * (num_cards-i) }
+  .sum
+
+# part 2
+
+players = input.split("\n\n").map { |p| p.lines.map(&:strip)[1..-1].map(&:to_i) }
+
+def self.recursive_combat(players, seen = Set.new)
+  # puts "new game #{players.map(&:size)}"
+
+  until players.any? { |p| p.size == 0 }
+    # puts players.inspect
+
+    return 0, players[0] if seen.include?(players) # we've been here before, player 0 wins this game
+    seen << players
+
+    recur_decider = players.map(&:first).each_with_index.select { |card, p| players[p].size-1 >= card }.map { |card, p| card }
+    if recur_decider.size == 2
+      # puts "recur #{recur_decider}"
+      round_winner, _ = recursive_combat(players.map(&:dup).each_with_index.map { |v, p| v.slice(1, recur_decider[p]) })
+      players[round_winner] << players[round_winner].shift
+      players[round_winner] << players[(round_winner+1)%2].shift
+      # puts "recur winner #{round_winner}"
+    else
+      round_winner = players.map(&:first).each_with_index.max[1]
+      players[round_winner] += players.map(&:shift).sort.reverse
+    end
+  end
+
+  game_winner = players.index { |p| p.size > 0 }
+  [game_winner, players[game_winner]]
+end
+
+puts recursive_combat(players)[1]
   .each_with_index
   .map { |v, i| v * (num_cards-i) }
   .sum
