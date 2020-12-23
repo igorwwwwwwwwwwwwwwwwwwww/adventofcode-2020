5c46f87f5fa9dbaa97fc08de6b56708ee1500c53
igor <igor@igor.io>
2020-12-23 14:56:42 +0100

    day 23, part 2
    ruby 23.rb  24.13s user 0.29s system 99% cpu 24.649 total

--- 23.rb
+++ 23.rb
@@ -56,28 +56,92 @@ puts (0..cups.size-1)
 
 puts "-"
 
+class LinkedListNode
+  attr_accessor :value
+  attr_accessor :next
+
+  def initialize(value, next_ = nil)
+    @value = value
+    @next = next_
+  end
+
+  def delete_next
+    deleted = @next
+    @next = deleted.next
+    deleted.value
+  end
+
+  def insert_next(value)
+    moved = @next
+    @next = LinkedListNode.new(value, moved)
+    @next
+  end
+
+  def each_node
+    yield self
+    head = self
+    current = head
+    while current.next != head
+      current = current.next
+      yield current
+    end
+  end
+
+  def inspect
+    a = to_a
+    "LinkedList<#{a.first..a.last}>"
+  end
+
+  def to_a
+    a = [@value]
+    head = self
+    current = head
+    while current.next != head
+      current = current.next
+      a << current.value
+    end
+    a
+  end
+
+  def self.from_a(a)
+    head = LinkedListNode.new(a.first)
+
+    prev = head
+    a[1..-1].each do |v|
+      current = LinkedListNode.new(v)
+      prev.next = current
+      prev = current
+    end
+
+    # circular
+    prev.next = head
+
+    head
+  end
+end
+
 cups = input.strip.chars.map(&:to_i)
 cups += (cups.max+1..1_000_000).to_a
 
 cups_min = cups.min
 cups_max = cups.max
 
-current = cups.first
-c = cups.index(current)
+list = LinkedListNode.from_a(cups)
+
+index = Array.new(cups.size)
+list.each_node do |node|
+  index[node.value] = node
+end
 
 10_000_000.times do |outer|
-  puts "#{Time.now} #{outer} processed" if outer % 1000 == 0
-  # puts "cups: #{cups}"
-  # puts "current: #{current}"
+  puts "#{Time.now} #{outer} processed" if outer % 1_000_000 == 0
+
+  current = list.value
 
   removed = []
-  removed << cups.delete_at((c+1) % (cups.size))
-  c = cups.index(current) unless cups[c] == current
-  removed << cups.delete_at((c+1) % (cups.size))
-  c = cups.index(current) unless cups[c] == current
-  removed << cups.delete_at((c+1) % (cups.size))
-
-  # puts "pick up: #{removed.inspect}"
+  removed << list.delete_next
+  removed << list.delete_next
+  removed << list.delete_next
 
   destination = current-1
   if destination < cups_min
@@ -89,17 +153,16 @@ c = cups.index(current)
       destination = cups_max
     end
   end
-  d = cups.index(destination)
 
-  removed.each_with_index do |cup, i|
-    cups.insert(d+i+1, cup)
-  end
+  d = index[destination]
+  d.insert_next(removed.shift)
+    .tap { |node| index[node.value] = node }
+    .insert_next(removed.shift)
+    .tap { |node| index[node.value] = node }
+    .insert_next(removed.shift)
+    .tap { |node| index[node.value] = node }
 
-  c = (cups.index(current)+1) % (cups.size)
-  current = cups[c]
-
-  # puts "destination: #{destination}"
-  # puts
+  list = list.next
 end
 
-puts cups.size
+puts index[1].next.value * index[1].next.next.value
